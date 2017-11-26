cc244ceb121aa21bb82736e9ca6b1121196f4a5c
debsrv <none@none.com>
2017-11-27 00:43:22 +0700

    + Backup all PostgreSQL databases in plaintext.

---
+++ backup_pg.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# Backup PostgreSQL DB.
+#
+
+# --------------------------------------------------------------------------------------------------------
+backup_path='/backup/db'
+now=`date +%Y%m%d_%H%M`
+gzip_level=8
+
+# --------------------------------------------------------------------------------------------------------
+
+
+echo "Dumping globals:"
+ sudo -u postgres pg_dumpall --globals-only | gzip -${gzip_level} | pv -terab  > ${backup_path}/${now}_pg_globals_only.sql.gz
+
+db_list=`sudo -u postgres psql -l | awk '{ print $1}' | grep -vE '^-|^List|^Name|^Список|^Имя|template[0|1]|^postgres$|^\||^\(|^$'`
+
+
+for i in ${db_list}; do
+    echo "Dumping db schema:" $i
+    sudo -u postgres pg_dump --schema-only --format=plain $i | gzip -${gzip_level} | pv -terab > ${backup_path}/${now}_${i}_schema.sql.gz
+    echo "Dumping db:       " $i
+    sudo -u postgres pg_dump --data-only --insert --format=plain $i | gzip -${gzip_level} | pv -terab > ${backup_path}/${now}_${i}_data.sql.gz
+done
+echo "Dump done!"
+
+
+
+# --------------------------------------------------------------------------------------------------------.
+
